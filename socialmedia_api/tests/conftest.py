5a0df5e4da56c78a48043bebe2c97a5105f95084
Håkon Skogland Halvorsen <hakon@alv.no>
2023-12-09 17:48:45 +0100

    Added initial tests

---
+++ socialmedia_api/tests/conftest.py
@@ -0,0 +1,34 @@
+from typing import AsyncGenerator, Generator
+
+import pytest
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
+
+from socialmedia_api.main import app
+from socialmedia_api.routers.post import comment_table, post_table
+
+
+@pytest.fixture(scope="session")  # Run once across all tests
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture()
+def client() -> Generator:
+    yield TestClient(app)
+
+
+# Clearing the tables before every test function
+@pytest.fixture(autouse=True)
+async def db() -> AsyncGenerator:
+    post_table.clear()
+    comment_table.clear()
+    yield
+
+
+@pytest.fixture()
+async def async_client(
+    client,
+) -> AsyncGenerator:  # Dependency injection of client - returns what client fixture returns
+    async with AsyncClient(app=app, base_url=client.base_url) as ac:
+        yield ac
